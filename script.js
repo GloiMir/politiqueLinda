a69584acde245f9bb8350ab0b665683b334f291b
GloiMir <gloirmirindi@gmail.com>
2025-06-20 22:32:05 +0200

    bouton sur liste

--- script.js
+++ script.js
@@ -5,24 +5,45 @@ async function fetchApiUrl() {
 }
 
 async function fetchElements() {
+
+    // Chargement des promoteurs
     const response = await fetch('https://lindabackend.onrender.com/api/sendPromoteurs');
     const elements = await response.json();
+
+    // Ouverture du lien de téléchargement
+    const openLink = (codePromo) => {
+        window.open(`https://lindapp.netlify.app?codePromo=${codePromo}`, '_blank');
+    };
+
     const container = document.getElementById('tableau');
+    
     elements.forEach(element => {
         const div = document.createElement('div');
-        div.classList.add('element');
-
-        // <img src='./MyIcon.png' alt="${element.nom}" class="image">
+        div.classList.add('content');
+    
+        // Créer le contenu sans le bouton
         div.innerHTML = `
-            <img src="${element.photo}" alt="${element.nom}" class="image">
-            <div class="info">
-                <span class="nom">${element.nom}</span>
-                <span class="code">${element.codePromo}</span>
-                <span class="phone">${element.telephone}</span>
-                <span class="phone">${element.email}</span>
+            <div class="element">
+                <img src="${element.photo}" alt="${element.nom}" class="image">
+                <div class="info">
+                    <span class="nom">${element.nom}</span>
+                    <span class="code">Code Promo : ${element.codePromo}</span>
+                    <span class="phone">${element.telephone}</span>
+                    <span class="phone">${element.email}</span>
+                    <span class="phone">${element.pays} / ${element.ville}</span>
+                </div>
             </div>
         `;
-
+    
+        // Créer le bouton séparément
+        const button = document.createElement('button');
+        button.className = 'bouton';
+        button.textContent = 'Créer dans son réseau';
+        button.addEventListener('click', () => openLink(element.codePromo));
+    
+        // Ajouter le bouton à la div
+        div.appendChild(button);
+    
         container.appendChild(div);
     });
 }
